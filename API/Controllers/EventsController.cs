0ddb8e0e8d1c2678f308dbcb08f066b0baf2d18f
Terry Brown <terrybrown@TerryBrownsMBP.lan>
2022-02-15 23:25:08 -0500

    Working session for .NET core
    Created a base API controller to eliminate duplication of code when creating future Controllers
    Created a basic controller for Events, which has  GetAll and GetByID functionality
    
    Added in Seed data for the local database.

---
+++ API/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class EventsController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public EventsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Event>>> GetEvents()
+        {
+            return await _context.Events.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Event>> GetEvent(Guid id)
+        {
+            return await _context.Events.FindAsync(id);
+        }
+    }
+}
