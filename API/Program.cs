1e463c47f74732c7b7df3b5b8e5f778e2a3fddc9
Terry Brown <terrybrown@TerryBrownsMBP.lan>
2022-02-16 23:37:52 -0500

    Added basic (happy-path only) CRUD operations to API
    Added MediatR
    Added AutoMapper

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ namespace API
                 await context.Database.MigrateAsync();
                 await Seed.SeedData(context);
             }
-            catch ( Exception ex)
+            catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occured during migration");
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,6 @@
+using Application.Core;
+using Application.Events;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Persistence;
@@ -29,13 +32,16 @@ namespace API
             });
 
             services.AddCors(opt =>
-           {
-               opt.AddPolicy("CorsPolicy", policy =>
-               {
-                   policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
 
-               });
-           });
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
