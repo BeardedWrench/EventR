dacb9c4b4207e7c18e6a2ec644c9acf31769b86d
Terry Brown <terrybrown@TerryBrownsMBP.lan>
2022-02-16 23:55:09 -0500

    Project Cleanup
    Moved our startup services into an extension class
    Minor file cleanup

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,47 +1,21 @@
-using Application.Core;
-using Application.Events;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Persistence;
-
+using API.Extensions;
 
 namespace API
 {
     public class Startup
     {
         private readonly IConfiguration _config;
+
         public Startup(IConfiguration config)
         {
             _config = config;
         }
 
-
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-
-                });
-            });
-
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(_config);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
